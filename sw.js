3ce280e0955e3dc755bdcfa1bc15be47dec8846a
martinbenisek-beny97 <martin.benisek@gmail.com>
2026-05-26 12:22:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'urban-jungle-v1';
+const CACHE_NAME = 'urban-jungle-v2';
 const ASSETS = ['./', './index.html', './index_v2.html', './manifest.json', './icons/icon.svg'];
 
 self.addEventListener('install', (event) => {
